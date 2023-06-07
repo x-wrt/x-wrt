ee1e03a63198efc6c955713a006f7e74529b343e
Doanduy09 <96837081+Doanduy09@users.noreply.github.com>
2023-06-07 13:14:36 +0700

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -23,5 +23,3 @@ echo 'src-git 3Ginfo https://github.com/4IceG/luci-app-3ginfo-lite.git' >>feeds.
 echo 'src-git Modem https://github.com/4IceG/luci-app-modemband.git' >>feeds.conf.default
 echo 'src-git SMS https://github.com/4IceG/luci-app-sms-tool.git' >>feeds.conf.default
 echo 'src-git atinout https://github.com/4IceG/luci-app-atinout-mod.git' >>feeds.conf.default
-echo 'CONFIG_FEED_atinout=y' >>.config
-echo 'CONFIG_PACKAGE_luci-app-atinout-mod=y' >>.config
