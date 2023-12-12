1e0749ad0cb96dc7dd94a2f450bb67369e779f2e
Chen Minqiang <ptpt52@gmail.com>
2023-12-12 23:04:31 +0800

    mediatek: add CMCC RAX3000M uboot layout

--- target/linux/mediatek/filogic/base-files/lib/upgrade/platform.sh
+++ target/linux/mediatek/filogic/base-files/lib/upgrade/platform.sh
@@ -255,6 +255,7 @@ platform_do_upgrade() {
 	acer,vero-w6m|\
 	airpi,ap3000m|\
 	arcadyan,mozart|\
+	cmcc,rax3000m-emmc-ubootlayout|\
 	glinet,gl-mt2500|\
 	glinet,gl-mt2500-airoha|\
 	glinet,gl-mt6000|\
@@ -465,6 +466,12 @@ platform_check_image() {
 		fit_check_image "$1"
 		return $?
 		;;
+	cmcc,rax3000m-emmc-ubootlayout|\
+	tenbay,ms3000k|\
+	tenbay,wr3000k-gsw-emmc-nor|\
+	tenda,ax12l-pro)
+		return 0
+		;;
 	creatlentem,clt-r30b1|\
 	creatlentem,clt-r30b1-112m|\
 	nradio,c8-668gl)
@@ -476,11 +483,6 @@ platform_check_image() {
 			return 1
 		}
 
-		return 0
-		;;
-	tenbay,ms3000k|\
-	tenbay,wr3000k-gsw-emmc-nor|\
-	tenda,ax12l-pro)
 		return 0
 		;;
 	*)
@@ -512,6 +514,7 @@ platform_copy_config() {
 	acer,vero-w6m|\
 	airpi,ap3000m|\
 	arcadyan,mozart|\
+	cmcc,rax3000m-emmc-ubootlayout|\
 	glinet,gl-mt2500|\
 	glinet,gl-mt2500-airoha|\
 	glinet,gl-mt6000|\
