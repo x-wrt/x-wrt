936022efaaf37fe00f89694a1e9c71f5c14de66e
Doanduy09 <96837081+Doanduy09@users.noreply.github.com>
2024-01-03 07:40:25 +0700

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -16,6 +16,3 @@ sed -i '/CONFIG_PACKAGE_glib2=y/d' .config
 echo 'CONFIG_FEED_atinout=y' >>.config
 echo 'CONFIG_PACKAGE_luci-app-atinout-mod=y' >>.config
 echo 'CONFIG_PACKAGE_luci-app-atcommands=y' >>.config
-echo 'pushd feeds/packages/lang
-rm -rf golang && svn co https://github.com/openwrt/packages/branches/openwrt-23.05/lang/golang
-popd
